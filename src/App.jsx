143f1358ee0369f06856322128c83920b61eba60
allpha23 <allpha23@gmail.com>
2023-03-25 18:20:34 -0300

    feat: Configurações iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Blog from './pages/Blog';
+import './styles/App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
